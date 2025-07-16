88ea808099d6c54b4fda2932895e319260585fec
Rishi Kant <kantrishi7779@gmail.com>
2025-07-17 00:45:46 +0530

    navbar fix

--- apps/web/src/components/navbars/Navbar.tsx
+++ apps/web/src/components/navbars/Navbar.tsx
@@ -1,12 +1,18 @@
 import AppLogo from "../app/AppLogo";
 import { Button } from "../ui/button";
+import { MdChevronRight } from "react-icons/md";
+
 
 export default function Navbar() {
+
     return (
         <div className="absolute top-4 left-1/2 -translate-x-1/2  w-full max-w-5xl px-4 py-4 rounded-lg shadow-lg border">
             <div className="mx-4 flex items-center justify-between">
                 <AppLogo />
-                <Button className="font-normal tracking-wide bg-dark-base">Sign in</Button>
+                <Button className="font-light text-sm tracking-wider bg-dark-base flex items-center justify-center">
+                    <span>Sign in</span>
+                    <MdChevronRight className="text-neutral-300" />
+                </Button>
             </div>
         </div>
     );
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,25 +1,25 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
-    content: [
-        "./app/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./pages/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                mono: ['"Geist Mono"', "monospace"],
-            },
-            colors: {
-                dark: {
-                    base: "#242424",
-                },
-            },
-        },
-    },
-    plugins: [],
-};
+// const config: Config = {
+//     content: [
+//         "./app/**/*.{ts,tsx}",
+//         "./components/**/*.{ts,tsx}",
+//         "./pages/**/*.{ts,tsx}",
+//         "./src/**/*.{ts,tsx}",
+//     ],
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 mono: ['"Geist Mono"', "monospace"],
+//             },
+//             colors: {
+//                 dark: {
+//                     base: "#242424",
+//                 },
+//             },
+//         },
+//     },
+//     plugins: [],
+// };
 
-export default config;
+// export default config;
