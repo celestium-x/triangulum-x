d0e1a66b86dd9d99501191839362b5a9b8f370c6
DipTheBeginner <dipthebeginner@gmail.com>
2025-07-17 19:14:09 +0530

    added tailwind utility for hiding scrollbar

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,11 +6,13 @@ import JoinQuizBar from "@/components/base/JoinQuizBar";
 export default function Home() {
   return (
     <div className={cn("w-full max-h-screen h-screen relative", "bg-light-base dark:bg-dark-primary")}>
-      <Navbar />
-      <div className="pt-30 h-full">
+      <Navbar/>
+      <div className="pt-30 h-full overflow-y-auto scrollbar-hide">
         <JoinQuizBar />
         <LandingPage />
       </div>
     </div>
   );
 }
+
+
