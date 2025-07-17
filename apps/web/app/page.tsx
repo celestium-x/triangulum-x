7330dde977bf2f945a531f4a62314ba07b8ec531
Rishi Kant <kantrishi7779@gmail.com>
2025-07-17 21:28:54 +0530

    removed build.yml

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ import LandingPage from "@/components/base/LandingPage";
 import JoinQuizBar from "@/components/base/JoinQuizBar";
 
 export default function Home() {
+
   return (
     <div className={cn("w-full max-h-screen h-screen relative", "bg-light-base dark:bg-dark-primary")}>
       <Navbar />
