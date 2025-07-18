259e33923a346cba8648dc9eb24b33a0e1a51c0b
Rishi Kant <kantrishi7779@gmail.com>
2025-07-18 19:08:08 +0530

    fixed prisma issue and added logo

--- apps/web/src/components/app/AppLogo.tsx
+++ apps/web/src/components/app/AppLogo.tsx
@@ -1,9 +1,9 @@
-import { TbSquareLetterTFilled } from "react-icons/tb";
+import Image from "next/image";
 
 export default function AppLogo() {
     return (
         <div className="flex items-center justify-center gap-x-2 cursor-pointer group">
-            <TbSquareLetterTFilled size={24} className="group-hover:-translate-x-1 transition-transform ease-in" />
+            <Image src={'/images/logo.jpg'} width={40} height={40} alt="logo" unoptimized className="rounded-sm" />
             <span className="text-xl text-neutral-900 dark:text-light-base font-medium">Triangulum</span>
         </div>
     )
