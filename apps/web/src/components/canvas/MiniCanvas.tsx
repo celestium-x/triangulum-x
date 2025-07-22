137d1a58a8e408dd5505c33fa5fbf5372bcefbde
Rishi Kant <kantrishi7779@gmail.com>
2025-07-23 02:06:02 +0530

    quiz creation ui halfway

--- apps/web/src/components/canvas/MiniCanvas.tsx
+++ apps/web/src/components/canvas/MiniCanvas.tsx
@@ -1,26 +1,37 @@
 import { Template } from "@/lib/templates";
 import { cn } from "@/lib/utils";
 import { QuestionType } from "@/types/prisma-types";
+import CanvasAccents from "../utility/CanvasAccents";
 
 interface MiniCanvasProps {
     template: Template | undefined;
     question: QuestionType;
     currentQuestionIndex: number;
+    questionIndex: number;
 }
 
-export default function MiniCanvas({ template, question, currentQuestionIndex }: MiniCanvasProps) {
+export default function MiniCanvas({ template, question, currentQuestionIndex, questionIndex }: MiniCanvasProps) {
     const selectedStyles = "border-2 border-[#5e59b3]";
+    
     return (
-        <div className={cn("w-full rounded-md h-16 p-0.5",
-            currentQuestionIndex === question.orderIndex && selectedStyles
-        )}>
+        <div
+            className={cn(
+                "w-full rounded-md h-18 p-0.5",
+                currentQuestionIndex === question.orderIndex && selectedStyles
+            )}
+            style={{ boxSizing: 'border-box' }}
+        >
             <div
                 style={{
                     backgroundColor: template?.background_color,
                     color: template?.text_color,
                 }}
-                className="w-full h-full rounded-sm flex items-center justify-center"
+                className="w-full h-full rounded-sm flex items-center justify-center overflow-hidden relative group"
             >
+                <div className="text-[5px] text-center bg-dark-base rounded-full absolute top-2 left-2 px-2 py-1 hidden group-hover:block">
+                    Question {questionIndex + 1}
+                </div>
+                <CanvasAccents design="slash" accentColor={template?.accent_color} />
                 <div className="text-xs">
                     {question.question.slice(0, 10)}...
                 </div>
--- apps/web/src/components/quiz/new/QuestionPallete.tsx
+++ apps/web/src/components/quiz/new/QuestionPallete.tsx
@@ -2,21 +2,29 @@ import MiniCanvas from "@/components/canvas/MiniCanvas";
 import { Button } from "@/components/ui/button";
 import UtilityCard from "@/components/utility/UtilityCard";
 import { templates } from "@/lib/templates";
+import { cn } from "@/lib/utils";
 import { useNewQuizStore } from "@/store/new-quiz/useNewQuizStore";
+import { TbPlus } from 'react-icons/tb'
 
 export default function QuestionPallete() {
     const { quiz, currentQuestionIndex } = useNewQuizStore();
     const currentQTemplate = templates.find(t => t.id === quiz.theme);
 
     return (
-        <UtilityCard className="max-w-[10rem] flex-shrink-0 shadow-none rounded-sm bg-neutral-200 dark:bg-dark-primary p-0 flex flex-col items-center px-1 border-none">
-            <Button className="bg-dark-base dark:bg-neutral-200 rounded-full w-[90%] m-0 mt-4 text-sm px-5">
-                Add Question
+        <UtilityCard className="max-w-[10rem] w-full flex-shrink-0 shadow-none rounded-sm bg-neutral-200 dark:bg-dark-primary p-0 flex flex-col items-center px-1 border-none">
+            <Button className={cn("bg-dark-base dark:bg-neutral-200 dark:hover:bg-light-base hover:bg-dark-primary ",
+                'rounded-full m-0 mt-4 px-20 text-xs font-light flex items-center justify-center gap-x-2'
+            )}>
+                <TbPlus />
+                <span>Add Question</span>
             </Button>
             <div className="flex flex-col gap-y-1.5 mt-6 w-full">
                 {
                     quiz.questions.map((question, idx) => (
-                        <MiniCanvas currentQuestionIndex={currentQuestionIndex} template={currentQTemplate} question={question} key={idx} />
+                        <div key={idx} className="flex items-end gap-x-2">
+                            <div className="text-xs">{idx + 1}.</div>
+                            <MiniCanvas currentQuestionIndex={currentQuestionIndex} template={currentQTemplate} question={question} questionIndex={idx} />
+                        </div>
                     ))
                 }
             </div>
