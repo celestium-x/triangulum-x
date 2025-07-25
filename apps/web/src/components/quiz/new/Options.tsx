c2f5091232a2190ca1c7e29b744a39817dfa4007
Piyush Raj <piyushraj26102004@gmail.com>
2025-07-25 18:25:15 +0530

    lint fixed

--- apps/web/src/components/quiz/new/Options.tsx
+++ apps/web/src/components/quiz/new/Options.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { templates } from "@/lib/templates";
 import { cn } from "@/lib/utils";
