4136ebbba408cd8dba882b27f38a6e6fc88576f7
Rishi Kant <kantrishi7779@gmail.com>
2025-07-27 04:44:14 +0530

    lint fix urghhhh

--- apps/web/src/components/base/DashBoard.tsx
+++ apps/web/src/components/base/DashBoard.tsx
@@ -1,19 +1,9 @@
 'use client';
-import { JSX, useState } from 'react';
+import { JSX } from 'react';
 import DashboardLeft from './DashboardLeft';
 import DashboardRight from './DashboardRight';
 
 export default function DashBoard(): JSX.Element {
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
-
-    function handleMouseEnter(): void {
-        setIsExpanded(true);
-    }
-
-    function handleMouseLeave(): void {
-        setIsExpanded(false);
-    }
-
     return (
         <div className="flex h-screen border-t-[1px] border-neutral-300 dark:border-neutral-700">
             <DashboardLeft />
