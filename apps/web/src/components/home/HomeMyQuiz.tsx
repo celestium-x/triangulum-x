fb25005ce285ce337d1a6f4839784782b046a583
Rishi Kant <kantrishi7779@gmail.com>
2025-07-26 16:45:21 +0530

    lint fix

--- apps/web/src/components/home/HomeMyQuiz.tsx
+++ apps/web/src/components/home/HomeMyQuiz.tsx
@@ -9,25 +9,26 @@ import AllQuizComponent from "../base/AllQuizComponent";
 export default function HomeMyQuiz() {
     const { session } = useUserSessionStore();
     const { setAllQuizs } = useAllQuizsStore();
-    async function getUserAllQuizs() {
-        if (!session?.user.token) return;
-        try {
-            const { data } = await axios.get(GET_ALL_OWNER_QUIZ_URL, {
-                headers: {
-                    Authorization: `Bearer ${session?.user.token}`
-                }
-            })
-            if (data.success) {
-                setAllQuizs(data.quizzes);
-            }
-        } catch (err) {
-            console.error("Error in getting all the quizzes", err);
-        }
-    }
 
     useEffect(() => {
+        async function getUserAllQuizs() {
+            if (!session?.user.token) return;
+            try {
+                const { data } = await axios.get(GET_ALL_OWNER_QUIZ_URL, {
+                    headers: {
+                        Authorization: `Bearer ${session?.user.token}`
+                    }
+                })
+                if (data.success) {
+                    setAllQuizs(data.quizzes);
+                }
+            } catch (err) {
+                console.error("Error in getting all the quizzes", err);
+            }
+        }
+        
         getUserAllQuizs();
-    }, [])
+    }, [session?.user.token, setAllQuizs])
 
     return (
         <div className="p-8">
