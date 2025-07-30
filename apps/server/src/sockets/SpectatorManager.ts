5a1d92213b498bf7685f1af58c5966350a780172
Anjan Suman <anjansuman80@gmail.com>
2025-07-31 01:30:39 +0530

    prettier fixes.

--- apps/server/src/sockets/SpectatorManager.ts
+++ apps/server/src/sockets/SpectatorManager.ts
@@ -1,20 +1,18 @@
-import Redis from "ioredis";
-import { CookiePayload, CustomWebSocket } from "../types/web-socket-types";
-import QuizManager from "./QuizManager";
-import prisma from "@repo/db/client";
-import { v4 as uuid } from "uuid";
-
+import Redis from 'ioredis';
+import { CookiePayload, CustomWebSocket } from '../types/web-socket-types';
+import QuizManager from './QuizManager';
+import prisma from '@repo/db/client';
+import { v4 as uuid } from 'uuid';
 
 interface SpectatorManagerDependencies {
-    publisher: Redis,
-    subscriber: Redis,
-    socketMapping: Map<string, CustomWebSocket>,
-    sessionSpectatorMapping: Map<string, string>,
-    quizManager: QuizManager
+    publisher: Redis;
+    subscriber: Redis;
+    socketMapping: Map<string, CustomWebSocket>;
+    sessionSpectatorMapping: Map<string, string>;
+    quizManager: QuizManager;
 }
 
 export default class SpectatorManager {
-
     private publisher: Redis;
     private subscriber: Redis;
     private socketMapping: Map<string, CustomWebSocket> = new Map<string, CustomWebSocket>();
@@ -31,15 +29,20 @@ export default class SpectatorManager {
 
     public async handle_connection(ws: CustomWebSocket, payload: CookiePayload): Promise<void> {
         const isValidSpectator = await this.validateSpectatorInDb(payload.quizId, payload.userId);
-        if(!isValidSpectator) {
+        if (!isValidSpectator) {
             ws.close();
             return;
         }
 
-        const existing_spectator_socket_id = this.sessionSpectatorMapping.get(payload.gameSessionId);
-        if(existing_spectator_socket_id) {
+        const existing_spectator_socket_id = this.sessionSpectatorMapping.get(
+            payload.gameSessionId,
+        );
+        if (existing_spectator_socket_id) {
             const spectator_existing_socket = this.socketMapping.get(existing_spectator_socket_id);
-            if(spectator_existing_socket && spectator_existing_socket.readyState === WebSocket.OPEN) {
+            if (
+                spectator_existing_socket &&
+                spectator_existing_socket.readyState === WebSocket.OPEN
+            ) {
                 spectator_existing_socket.close();
             }
         }
@@ -50,15 +53,14 @@ export default class SpectatorManager {
         this.sessionSpectatorMapping.set(payload.gameSessionId, ws.id);
         // this.quizManager.onSpectatorConnect(payload.gameSessionId, ws.id);
         // this.setup_message_handlers(ws);
-
     }
 
     private async validateSpectatorInDb(quizId: string, spectatorId: string): Promise<boolean> {
         const spectator = await prisma.spectator.findUnique({
             where: {
                 id: spectatorId,
-                quizId: quizId
-            }
+                quizId: quizId,
+            },
         });
 
         return !!spectator;
@@ -67,5 +69,4 @@ export default class SpectatorManager {
     private generateSocketId(): string {
         return uuid();
     }
-
 }
