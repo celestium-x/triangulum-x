43617dca2aa5eda86b536578596d32e60e600368
DipTheBeginner <dipthebeginner@gmail.com>
2025-07-30 21:33:04 +0530

    Ui design for Review page

--- apps/web/src/components/content/HeadAndSubHead.tsx
+++ apps/web/src/components/content/HeadAndSubHead.tsx
@@ -1,13 +1,19 @@
+import { cn } from "@/lib/utils";
+
 interface HeadAndSubHeadProps extends React.HTMLAttributes<HTMLDivElement> {
     heading: string;
     subHeading: string;
     className?: string;
+    headClassname?: string,
+    subHeadClassname?: string,
 }
 
 export default function HeadAndSubHead({
     heading,
     subHeading,
     className,
+    headClassname,
+    subHeadClassname,
     ...props
 }: HeadAndSubHeadProps) {
     return (
@@ -15,8 +21,8 @@ export default function HeadAndSubHead({
             {...props}
             className={`flex flex-col items-start justify-center dark:text-neutral-300 text-neutral-800 ${className ?? ''}`}
         >
-            <span className="text-lg font-bold">{heading}</span>
-            <span className="font-normal text-sm">{subHeading}</span>
+            <span className={cn("text-lg font-bold", headClassname)}>{heading}</span>
+            <span className={cn("font-normal text-sm", subHeadClassname)}>{subHeading}</span>
         </div>
     );
 }
