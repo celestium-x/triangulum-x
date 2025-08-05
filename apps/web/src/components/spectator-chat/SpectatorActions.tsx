d60bc2f5d5f1e097e4b0b125ca319faea28ce05e
DipTheBeginner <dipthebeginner@gmail.com>
2025-08-05 23:42:12 +0530

    spectator-preview screen

--- apps/web/src/components/spectator-chat/SpectatorActions.tsx
+++ apps/web/src/components/spectator-chat/SpectatorActions.tsx
@@ -39,7 +39,7 @@ export default function SpectatorActions({ onChatExpandChange }: SpectatorAction
 
     useEffect(() => {
         onChatExpandChange?.(isExpanded);
-    }, [isExpanded]);
+    }, [isExpanded,onChatExpandChange]);
 
     const users: User[] = useMemo(() => {
         const globalChat: User = {
