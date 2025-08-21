5b074052f51b4cfe84198b55aa08c2ff5e055260
Piyush Raj <piyushraj26102004@gmail.com>
2025-08-21 19:37:02 +0530

    subscribe event handler class added

---
+++ apps/web/src/lib/subscribe-event-handlers.ts
@@ -0,0 +1,108 @@
+import { useLiveParticipantsStore } from "@/store/live-quiz/useLiveParticipantsStore";
+import { useLiveQuizGlobalChatStore } from "@/store/live-quiz/useLiveQuizGlobalChatStore";
+import { useLiveQuizStore } from "@/store/live-quiz/useLiveQuizStore";
+import { useLiveParticipantStore, useLiveSpectatorStore } from "@/store/live-quiz/useLiveQuizUserStore";
+import { useLiveSpectatorsStore } from "@/store/live-quiz/useLiveSpectatorsStore";
+import { GameSessionType, ParticipantType, SpectatorType } from "@/types/prisma-types";
+import { ChatMessageType, ChatReactionType } from "@/types/web-socket-types";
+
+
+export class SubscribeEventHandlers {
+
+    // participant-evenets
+
+    static handleIncomingMessage(payload: unknown) {
+        const { upsertParticipant } = useLiveParticipantsStore.getState();
+        upsertParticipant(payload as ParticipantType);
+    }
+
+    static handleIncomingNameChangeMessage(payload: unknown) {
+        const { upsertParticipant } = useLiveParticipantsStore.getState();
+        const { updateParticipantData } = useLiveParticipantStore.getState();
+
+        const message = payload as ParticipantType;
+        upsertParticipant({
+            id: message.id,
+            nickname: message.nickname,
+            isNameChanged: true,
+        } as ParticipantType);
+        updateParticipantData({
+            id: message.id,
+            nickname: message.nickname,
+            isNameChanged: true,
+        } as ParticipantType);
+
+    }
+
+
+    // game-session-events
+
+    static handleIncomingQuestionPreviewPageChange(payload: unknown) {
+        const { updateGameSession } = useLiveQuizStore.getState();
+        const message = payload as GameSessionType;
+        updateGameSession({ id: message.id, hostScreen: message.hostScreen } as GameSessionType);
+    }
+
+
+    // spectator-events
+
+    static handleSpectatorNameChangeMessage(payload: unknown) {
+        const { upsertSpectator } = useLiveSpectatorsStore.getState();
+        const { updateSpectatorData } = useLiveSpectatorStore.getState();
+        const message = payload as SpectatorType;
+
+        upsertSpectator({
+            id: message.id,
+            nickname: message.nickname,
+            isNameChanged: true,
+        } as SpectatorType);
+        updateSpectatorData({
+            id: message.id,
+            nickname: message.nickname,
+            isNameChanged: true,
+        } as SpectatorType);
+
+    }
+
+    static handleIncomingNewSpectator(payload: unknown) {
+        const { upsertSpectator } = useLiveSpectatorsStore.getState();
+        upsertSpectator(payload as SpectatorType);
+    }
+
+
+    // chat-events 
+
+    static handleIncomingChatReactionMessage(payload: unknown) {
+        const { addChatReaction } = useLiveQuizGlobalChatStore.getState();
+        const message = payload as ChatReactionType;
+
+        addChatReaction(message);
+    }
+
+    static handleIncomingChatMessage(payload: unknown) {
+        const { spectatorData } = useLiveSpectatorStore.getState();
+        const { addChatMessage } = useLiveQuizGlobalChatStore.getState();
+        const messagePayload  = payload as { id: string; payload: ChatMessageType };
+        const chat = messagePayload.payload;
+        
+        if (chat.senderId === spectatorData?.id) return;
+        
+        addChatMessage(chat);
+    }
+    
+    static handleIncomingReactionEvent(payload: unknown) {
+        const { spectatorData } = useLiveSpectatorStore.getState();
+        const { addChatReaction } = useLiveQuizGlobalChatStore.getState();
+        
+        const reactionPayload = payload as { id: string; payload: ChatReactionType };
+        const reaction = reactionPayload.payload;
+        if (
+            reaction.reactorName === spectatorData?.nickname &&
+            reaction.reactorType === 'SPECTATOR'
+        )
+        return;
+        addChatReaction(reaction);
+    }
+
+    static handleHostLaunchQuestion() {}
+}
