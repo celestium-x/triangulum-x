acedc4ed194de8c1cfa448f0931d249a4f3d1523
Piyush Raj <piyushraj26102004@gmail.com>
2025-08-31 22:58:17 +0530

    ui: footer and CustomComponent at landing page

--- apps/web/src/components/base/FeatureArcComponent.tsx
+++ apps/web/src/components/base/FeatureArcComponent.tsx
@@ -2,7 +2,7 @@ import { ArcTimelineDemo } from '../ui/ArcTimeline';
 
 export default function FeatureArcComponent() {
     return (
-        <div className="overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)] [-webkit-mask-image:linear-gradient(to_right,transparent,black,transparent)] mt-[12rem] select-none h-full relative">
+        <div className="overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)] [-webkit-mask-image:linear-gradient(to_right,transparent,black,transparent)] mt-50 select-none h-full relative">
             <div className="w-full flex items-center justify-center gap-x-2">
                 <span className="text-5xl text-transparent font-semibold bg-clip-text bg-gradient-to-b dark:from-neutral-100 via-neutral-400 to-neutral-900">
                     Where Knowledge Meets
