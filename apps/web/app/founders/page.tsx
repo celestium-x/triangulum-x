58eaf13805d8b46d0051a71409f15c5620ed1dc0
Piyush Raj <piyushraj26102004@gmail.com>
2025-09-02 02:36:39 +0530

    ui: founders page complete

--- apps/web/app/founders/page.tsx
+++ apps/web/app/founders/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import HomeScreenFooter from '@/components/base/HomeScreenFooter';
 import FounderBase from '@/components/developers/FounderBase';
 
@@ -6,6 +7,14 @@ export default function Founders() {
         <div className="relative w-screen h-screen">
             <FounderBase />
             <HomeScreenFooter />
+=======
+import FounderBase from '@/components/founders/FounderBase';
+
+export default function Founders() {
+    return (
+        <div className="max-w-screen h-screen custom-scrollbar">
+                <FounderBase />
+>>>>>>> b17b205 (ui: founders page complete)
         </div>
     );
 }
